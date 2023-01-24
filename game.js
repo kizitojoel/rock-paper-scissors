ca504ace83c30aafc4502075a8b1fd6275432a80
Joel Kizito <kizitojoel16@gmail.com>
2023-01-24 23:19:57 +0300

    Create game skeleton and define the random picker

---
+++ game.js
@@ -0,0 +1,13 @@
+// Declare function to pick a random computer play
+function getComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+console.log(getComputerChoice());
+
+//Define a function that takes both user play and random computer play, and determines the winner using if statements
+
+
+//Display the winner onto the screen
+
+//Define a game function that loops through the playRound function five times and keeps the score
